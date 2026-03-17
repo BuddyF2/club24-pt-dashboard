4e4a47068b24b680b454e38ce9dd6a71b8e16f3f
BuddyF2 <feedsatwork@gmail.com>
2026-03-16 23:50:39 -0400

    Update abc_api.py

--- abc_api.py
+++ abc_api.py
@@ -1,3 +1,5 @@
+# abc_api.py
+
 import requests
 import streamlit as st
 
@@ -6,12 +8,13 @@ APP_ID = st.secrets["ABC_APPLICATION_ID"]
 APP_KEY = st.secrets["ABC_APPLICATION_KEY"]
 
 HEADERS = {
-    "Accept": "application/json",
+    "accept": "application/json",
     "app_id": APP_ID,
     "app_key": APP_KEY,
 }
 
-def abc_get(endpoint: str, params: dict | None = None) -> dict:
+
+def abc_get(endpoint: str, params: dict | None = None):
     response = requests.get(
         f"{BASE_URL}/{endpoint.lstrip('/')}",
         headers=HEADERS,
